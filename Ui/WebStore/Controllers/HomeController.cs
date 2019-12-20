01d32b85d247b7a5e8aa839349388681182bd726
Dant777 <kolbt87@gmail.com>
2019-12-20 18:52:19 +0300

    Lesson 4: Built-in logging: the basics

--- Ui/WebStore/Controllers/HomeController.cs
+++ Ui/WebStore/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using WebStore.Infrastructure;
 using WebStore.Interfaces;
 
@@ -12,15 +13,26 @@ namespace WebStore.Controllers
     public class HomeController : Controller
     {
         private readonly IValueService _valueService;
+        private readonly ILogger<HomeController> _logger;
 
-        public HomeController(IValueService valueService)
+        public HomeController(IValueService valueService, ILogger<HomeController> logger)
         {
             _valueService = valueService;
+            _logger = logger;
         }
 
         [SimpleActionFilter]
         public async Task<IActionResult> Index()
         {
+
+            _logger.LogInformation("Index action requested");
+            _logger.LogTrace("Trace! winter is coming");
+            _logger.LogInformation("Info! winter is coming");
+            _logger.LogWarning("Warning! winter is coming");
+            _logger.LogDebug("Debug! winter is coming");
+            _logger.LogError("Error! winter is coming");
+            _logger.LogCritical("Critical! winter is coming");
+
             var values = await _valueService.GetAsync();
             return View(values);
 
